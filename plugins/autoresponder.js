176ac5cb2b7b50c42f849cbfac0eb14d2645f1b7
D.E.S.N_OFFICIAL17 <131302001+Saldarriaga184@users.noreply.github.com>
2023-06-09 14:11:48 -0500

    Actualizar autoresponder.js

--- plugins/autoresponder.js
+++ plugins/autoresponder.js
@@ -7,7 +7,7 @@ let chat = global.db.data.chats[m.chat]
 if (m.mentionedJid.includes(this.user.jid) && m.isGroup && !chat.isBanned) {
 let stiker = await sticker(imagen1, false, global.packname, global.author)  
 this.sendFile(m.chat, stiker, 'sticker.webp', null, m, false, { 
-contextInfo: { externalAdReply: { title: '⫷᭄𝙆𝙖𝙣𝙖𝙠𝙞𝘽𝙤𝙩-𝙐𝙡𝙩𝙧𝙖✍', body: '©Saldarriaga184 ﹏✍', sourceUrl: `https://github.com/Saldarriaga184/KanakiBot-MD `, thumbnail: imagen2}}})}
+contextInfo: { externalAdReply: { title: '⫷᭄𝙆𝙖𝙣𝙖𝙠𝙞𝘽𝙤𝙩-𝙐𝙡𝙩𝙧𝙖✍', body: '©Saldarriaga184 ﹏✍', sourceUrl: `https://github.com/Saldarriaga184/KanekiBot-MD `, thumbnail: imagen2}}})}
     
 return !0 }
 export default handler
